1fcbbd3c08e712b894244f935d3a9facd5930730
Bob Remeika <bob@ragie.ai>
2024-10-09 14:09:02 -0700

    Add index timing and reformat

--- rag_demo/__main__.py
+++ rag_demo/__main__.py
@@ -1,11 +1,16 @@
 import argparse
 import os
+import time
 
 from openai import OpenAI
 import psycopg
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--skip-reload', action='store_true', help='Skip the reload process if this flag is provided.')
+parser.add_argument(
+    "--skip-reload",
+    action="store_true",
+    help="Skip the reload process if this flag is provided.",
+)
 args = parser.parse_args()
 
 DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data")
@@ -13,19 +18,24 @@ CHUNK_SIZE = 2048
 
 opeani = OpenAI()
 
-database_url = os.environ.get("DATABASE_URL", "postgresql://postgres:postgres@localhost:6432/rag_demo")
+database_url = os.environ.get(
+    "DATABASE_URL", "postgresql://postgres:postgres@localhost:6432/rag_demo"
+)
 db = psycopg.Connection.connect(database_url)
+
 
 # This is very naive chunking. LangChain has excellent chunking libraries.  Do not use
 # this technique in production as it will yield very bad results.
 def split_string_by_length(input_string, length):
     return [input_string[i : i + length] for i in range(0, len(input_string), length)]
 
+
 # Loop through files in the data directory and create embeddings in the database
 if not args.skip_reload:
     print("Cleaning database...")
     db.execute("TRUNCATE TABLE items")
 
+    tic = time.perf_counter()
     for filename in os.listdir(DATA_DIR):
         file_path = os.path.join(DATA_DIR, filename)
 
@@ -39,10 +49,12 @@ if not args.skip_reload:
             )
             print("Creating embedding...", len(chunk))
             db.execute(
-                "INSERT INTO items (embedding, chunk) VALUES (%s, %s)", [str(response.data[0].embedding), chunk]
+                "INSERT INTO items (embedding, chunk) VALUES (%s, %s)",
+                [str(response.data[0].embedding), chunk],
             )
-    db.commit()
 
+    print(f"\nTotal index time: {time.perf_counter() - tic}ms")
+    db.commit()
 
 question = input("\nEnter question: ")
 
@@ -55,7 +67,10 @@ response = opeani.embeddings.create(
 )
 
 embedding = str(response.data[0].embedding)
-result = db.execute("SELECT 1 - (embedding <=> %s) as score, chunk FROM items ORDER BY embedding <-> %s LIMIT 5", [embedding, embedding])
+result = db.execute(
+    "SELECT 1 - (embedding <=> %s) as score, chunk FROM items ORDER BY embedding <-> %s LIMIT 5",
+    [embedding, embedding],
+)
 rows = list(result)
 
 
@@ -73,20 +88,14 @@ Question: {question}
 response = opeani.chat.completions.create(
     model="gpt-4o",
     messages=[
-        {
-            "role": "system",
-            "content": "You are a helpful assisstant."
-        },
-        {
-            "role": "user",
-             "content": [{"type": "text", "text": prompt}]
-        }
-    ]
+        {"role": "system", "content": "You are a helpful assisstant."},
+        {"role": "user", "content": [{"type": "text", "text": prompt}]},
+    ],
 )
 
 print(f"\nUsing {len(rows)} chunks in answer. Answer:\n")
 print(response.choices[0].message.content)
 
 view_prompt = input("\nWould you like to see the raw prompt? [Yn] ")
-if view_prompt =="Y":
+if view_prompt == "Y":
     print("\n" + prompt)
