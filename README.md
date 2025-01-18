ed0bf06535ca49299deb07664eedab228b63a8e4
Sergio Ferro <Sergio_ferro@epam.com>
2025-01-18 15:20:37 -0300

    feat: adding paper and example_env file, found bug risk in transformers killing the process, pending move to API

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ A bare bones RAG application for educational purposes.
 
 DISCLAIMER: There are several concepts in this repository that can be implemented in much better ways.  The point of this repository is to remove unfamiliar terms and abstractions as much as possible to demonstrate the essential concepts of a RAG application.
 
-For a production ready RAG solution, check out https://ragie.ai
+You should get acquainted first with RAG and [when to use it and when not to.](https://www.anthropic.com/news/contextual-retrieval)
+Also, feel free to check out [NV-EMbed-v2](https://huggingface.co/nvidia/NV-Embed-v2) an embedding model that has been specifically fine tuned for retrieval tasks. 
 
 ## Sample output
 ```
@@ -12,44 +13,22 @@ Cleaning database...
 Creating embedding... 2048
 Creating embedding... 2048
 Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 2048
-Creating embedding... 468
 
 Total index time: 11.419859999790788ms
 
-Enter question: Why query reranking better than regular semantic search?
+Enter question: What are the main contributions from the paper?
 scores:  [0.4631096466649033, 0.4358613307724073, 0.4234167246123255, 0.41794766951856144, 0.4139573872089386]
 
 Using 5 chunks in answer. Answer:
 
 Query reranking is considered better than regular semantic search in this context because it involves the adaptation of search queries themselves to bridge the gap between the input text and the needed knowledge. The use of a trainable rewriter, as described, helps in refining the search query to better align it with the context needed by a large language model (LLM) reader. This process results in a higher snippet hit rate and improved retrieval effectiveness compared to standard retrieval methods. The usage of techniques like BM25 for content selection is shown to recall better documents than just relying on snippets. Furthermore, query rewriting significantly enhances the retriever's performance compared to the reader, suggesting that refining the search query can lead to more accurate and relevant retrievals, hence proving more effective than traditional semantic search methods.
 
-Would you like to see the raw prompt? [Yn]
+Would you like to see the raw prompt? [Y/n]
 ```
 
 ## Setup:
+
+This setup works for my WSL installation of Ubuntu with root access.
 
 Start a pgvector docker container:
 
@@ -59,11 +38,21 @@ docker run -p 6432:5432  --name pgvector -e POSTGRES_PASSWORD=postgres -d pgvect
 
 Note: The host port for the docker container is 6432 instead of the normal 5432 to avoid port collisions
 
+Optional:
+
+
 Setup the database:
 ```
 psql -h localhost -p 6432 -U postgres -c "CREATE DATABASE rag_demo;"
 psql -h localhost -p 6432 -U postgres rag_demo < schema.sql
 ```
+*Troubleshoot*: 
+```
+sudo apt update
+sudo apt install postgresql-client-common
+sudo apt install postgresql-client
+```
+
 
 Export your Hugging Face API Key environment variable:
 ```
@@ -74,8 +63,19 @@ Poetry install:
 ```
 poetry install
 ```
+*Troubleshoot*: 
+```
+sudo apt update
+curl -fsSL https://pyenv.run | bash
+pyenv install 3.12.0
+pyenv local 3.12.0
+```
 
 ### Running
 
 `poetry run python -m rag_demo`
 
+### Check chunks table first 5 rows
+```
+psql -h localhost -p 6432 -U postgres rag_demo -c "SELECT * FROM chunks LIMIT 5;"
+```
--- rag_demo/__main__.py
+++ rag_demo/__main__.py
@@ -1,57 +1,62 @@
-import argparse
+### Utility libraries
 import os
 import time
 
-from openai import OpenAI
+### PostgreSQL adapter for Python
 import psycopg
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--skip-reload",
-    action="store_true",
-    help="Skip the reload process if this flag is provided.",
-)
-args = parser.parse_args()
+### PyPDF for text extraction
+from PyPDF2 import PdfReader
 
+### Embeddings model
+import torch
+import torch.nn.functional as F
+from transformers import AutoModel
+
+### Text generation model
+from transformers import pipeline
+
+### Constants
 DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data")
-CHUNK_SIZE = 2048
+CHUNK_SIZE = 512
+MODEL_ID = "meta-llama/Llama-3.2-3B"
 
-opeani = OpenAI()
-
+### PostgreSQL database url and connection
 database_url = os.environ.get(
     "DATABASE_URL", "postgresql://postgres:postgres@localhost:6432/rag_demo"
 )
 db = psycopg.Connection.connect(database_url)
 
 
-# This is very naive chunking. LangChain has excellent chunking libraries.  Do not use
+# This is very naive chunking, just to show the concept. LangChain/LlamaIndex have excellent chunking libraries.  Do not use
 # this technique in production as it will yield very bad results.
 def split_string_by_length(input_string, length):
     return [input_string[i : i + length] for i in range(0, len(input_string), length)]
 
 
-# Loop through files in the data directory and create embeddings in the database
-if not args.skip_reload:
-    print("Cleaning database...")
-    db.execute("TRUNCATE TABLE items")
+# Loop through chunks from the pdf and create embeddings in the database
 
-    tic = time.perf_counter()
-    for filename in os.listdir(DATA_DIR):
-        file_path = os.path.join(DATA_DIR, filename)
+print("Cleaning database...")
+db.execute("TRUNCATE TABLE chunks")
 
-        with open(file_path, "r") as file:
-            content = file.read()
+# load model with tokenizer
+model = AutoModel.from_pretrained('nvidia/NV-Embed-v2', trust_remote_code=True)
 
-        for chunk in split_string_by_length(content, CHUNK_SIZE):
-            response = opeani.embeddings.create(
-                model="text-embedding-3-large",
-                input=chunk,
-            )
-            print("Creating embedding...", len(chunk))
-            db.execute(
-                "INSERT INTO items (embedding, chunk) VALUES (%s, %s)",
-                [str(response.data[0].embedding), chunk],
-            )
+tic = time.perf_counter()
+for filename in os.listdir(DATA_DIR):
+    file_path = os.path.join(DATA_DIR, filename)
+
+    reader = PdfReader(file_path)
+    content = ""
+    for page in reader.pages:
+        content += page.extract_text()
+
+    for chunk in split_string_by_length(content, CHUNK_SIZE):
+        print(f"Creating embedding for chunk: {chunk[20]}...")
+        
+        db.execute(
+            f"INSERT INTO items (embedding, chunk) VALUES ({model.encode(chunk)}, {chunk})",
+        )
 
     print(f"\nTotal index time: {time.perf_counter() - tic}ms")
     db.commit()
@@ -61,18 +66,15 @@ question = input("\nEnter question: ")
 # Create embedding from question.  Many RAG applications use a query rewriter before querying
 # the vector database.  For more information on query rewriting, see this whitepaper:
 #    https://arxiv.org/abs/2305.14283
-response = opeani.embeddings.create(
-    model="text-embedding-3-large",
-    input=question,
-)
+question_embedding = model.encode(question)
 
-embedding = str(response.data[0].embedding)
 result = db.execute(
-    "SELECT 1 - (embedding <=> %s) as score, chunk FROM items ORDER BY embedding <-> %s LIMIT 5",
-    [embedding, embedding],
+    f"""SELECT (embedding <=> {question_embedding})*100 as score, chunk 
+    FROM items 
+    ORDER BY score DESC
+    LIMIT 5"""
 )
 rows = list(result)
-
 
 print("scores: ", [row[0] for row in rows])
 context = "\n\n".join([row[1] for row in rows])
@@ -85,17 +87,18 @@ Answer the question using only the following context:
 Question: {question}
 """
 
-response = opeani.chat.completions.create(
-    model="gpt-4o",
-    messages=[
-        {"role": "system", "content": "You are a helpful assisstant."},
-        {"role": "user", "content": [{"type": "text", "text": prompt}]},
-    ],
+pipe = pipeline(
+    "text-generation", 
+    model=MODEL_ID,
+    torch_dtype=torch.bfloat16,
+    device_map="auto"
 )
 
-print(f"\nUsing {len(rows)} chunks in answer. Answer:\n")
-print(response.choices[0].message.content)
+answer = pipe(prompt)
 
-view_prompt = input("\nWould you like to see the raw prompt? [Yn] ")
+print(f"\nUsing {len(rows)} chunks in answer. Answer:\n")
+print(answer[0]['generated_text'])
+
+view_prompt = input("\nWould you like to see the raw prompt? [Y/N] ")
 if view_prompt == "Y":
     print("\n" + prompt)
